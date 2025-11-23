dc3e7c9d61fad132599266a7d7857504c4fc3a4c
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-23 21:52:00 +0545

    add initial module and HTML setup with ES6 imports

---
+++ module.js
@@ -0,0 +1,12 @@
+/*ES6.Module = An external file that contains reusable code
+that can be imported into other JavaScript files.
+Write reusable code for many different apps.
+Can contain variables, classes, functions ... and more
+Introduced as part of ECMAScript 2015 update
+*/
+
+import { age, increment, decrement } from "/module1.js";
+
+console.log(age);
+console.log(increment(10));
+console.log(decrement(11));
---
+++ module1.js
@@ -0,0 +1,10 @@
+export let age = 9;
+
+export function increment(num) {
+  num++;
+  return num;
+}
+export function decrement(num) {
+  num--;
+  return num;
+}
