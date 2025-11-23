fd2cae81685dd0f0e9b5fa8ef89442332b224690
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-23 22:58:32 +0545

    refactor error handling to use YoungAgeError class for age validation

--- errHandling.js
+++ errHandling.js
@@ -1,24 +1,28 @@
-class YoungAgeError {
+class YoungAgeError extends Error {
+  constructor(msg) {
+    super(msg);
+    this.name = "YoungAgeError";
+  }
+}
+class Person {
   constructor(age) {
     this.age = age;
   }
   checkAge() {
     if (this.age > 18) {
-      console.log("Can vote");
+      console.log(`Can vote`);
     } else {
-      throw new Error("Can't Vote");
+      throw new YoungAgeError(`Your age is ${this.age}. so, You Can't Vote`);
     }
   }
 }
-
 try {
-  let youngygeerror = new YoungAgeError(4);
-  youngygeerror.checkAge();
+  let person = new Person(4);
+  person.checkAge();
 } catch (error) {
-  console.error(error);
+  console.error(error.name + ": " + error.message);
 }
 console.log("done");
-
 
 // try {
 //   console.lag("jfd");
